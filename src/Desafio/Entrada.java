a8c4b127eb4ad73e6c311554ff151e73f43e4059
Jose Antonio <anthony_prz@hotmail.com>
2018-10-03 11:23:54 -0500

    Se agrego nuevos metodos

--- src/Desafio/Entrada.java
+++ src/Desafio/Entrada.java
@@ -1,4 +1,14 @@
 package Desafio;
 
 public class Entrada {
+
+    public static void main(String[] args)   {
+
+            Problema  entrada = new Problema();
+           // Problema entrada1 = new Problema("Hola Mundo");
+            System.out.println(entrada.getEntrada());
+
+
+    }
+
 }
--- src/Desafio/Problema.java
+++ src/Desafio/Problema.java
@@ -3,10 +3,29 @@ package Desafio;
 import mx.com.sintelti.desafios.oporx.Solucion;
 
 public class Problema implements Solucion {
-    @Override
-    public String convertir (String entrada){
-        return null;
+    private String  entrada;
+    public Problema(){
+        entrada="Desconocida";
+        entrada= convertir(entrada);
 
     }
+
+    @Override
+    public String convertir (String entrada) {
+
+        entrada = entrada.replaceAll("o", "x");
+       return entrada;
+
+    }
+
+
+        public String getEntrada () {
+            return entrada;
+
+        }
+        public void setEntrada (String entrada){
+            this.entrada = entrada;
+        }
+
 
 }
