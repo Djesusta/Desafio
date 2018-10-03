1a207dfbd72e24432494026be882ee2e56a5ab06
Jose Antonio <anthony_prz@hotmail.com>
2018-10-03 10:25:44 -0500

    Se agrego la interfaz

---
+++ src/Desafio/Problema.java
@@ -0,0 +1,12 @@
+package Desafio;
+
+import mx.com.sintelti.desafios.oporx.Solucion;
+
+public class Problema implements Solucion {
+    @Override
+    public String convertir (String entrada){
+        return null;
+
+    }
+
+}
